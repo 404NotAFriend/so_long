1234df390b691091e384dd82bb30145cb63affa6
404NotAFriend <brunodrcgomes@gmail.com>
2026-01-03 09:40:47 +0000

    added function to input handler.c , edit main.c and map_init and created xpm files for game from the tilesets , put them inside texture folder

--- src/input_handler.c
+++ src/input_handler.c
@@ -6,8 +6,14 @@
 /*   By: bramalho@student.42porto.com <bramalho>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 11:04:27 by bramalho@st       #+#    #+#             */
-/*   Updated: 2025/12/16 11:04:28 by bramalho@st      ###   ########.fr       */
+/*   Updated: 2026/01/03 09:11:15 by bramalho@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+int	close_window(t_game *game)
+{
+	cleanup_and_exit(game, 0);
+	return (0);
+}
--- src/map_initialization.c
+++ src/map_initialization.c
@@ -6,8 +6,21 @@
 /*   By: bramalho@student.42porto.com <bramalho>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 11:04:20 by bramalho@st       #+#    #+#             */
-/*   Updated: 2025/12/16 11:04:21 by bramalho@st      ###   ########.fr       */
+/*   Updated: 2026/01/03 09:10:10 by bramalho@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+void	init_game(t_game *game)
+{
+	game->gfx.mlx = mlx_init();
+	if (!game->gfx.mlx)
+		error_exit(ERR_MLX_INIT);
+	game->gfx.window = mlx_new_window(game->gfx.mlx,
+			game->map.width * TILE_SIZE,
+			game->map.height * TILE_SIZE,
+			"so_long");
+	if (!game->gfx.window)
+		error_exit(ERR_WINDOW);
+}
